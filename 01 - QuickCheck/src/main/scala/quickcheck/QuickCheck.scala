d75cbfaaf9a00c89afe305ac614c92e325f221cc
Samir Talwar <samir@noodlesandwich.com>
2013-11-12 22:58:16 +0000

    Assignment #1: When generating the heap, arbitrarily delete some items.

--- 01 - QuickCheck/src/main/scala/quickcheck/QuickCheck.scala
+++ 01 - QuickCheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -20,7 +20,7 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     else
       findMin(heap) == b
   }
-  
+
   property("inserting another value equal to the minimum value does not change the minimum value") = forAll { heap: H =>
     val min =
       if (isEmpty(heap)) 0
@@ -69,8 +69,13 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
   lazy val genHeap: Gen[H] = Gen.sized { size =>
     for {
-      items <- listOfN(size, posNum[Int])
-    } yield items.foldLeft(empty)((heap, value) => insert(value, heap))
+      extraItemCount <- choose(0, size)
+      items <- listOfN(size + extraItemCount, posNum[Int])
+    } yield {
+      val heapWithExtras = items.foldLeft(empty)((heap, value) => insert(value, heap))
+      val heapOfTheCorrectSize = (0 until extraItemCount).foldLeft(heapWithExtras)((heap, _) => deleteMin(heap))
+      heapOfTheCorrectSize
+    }
   }
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
