a01975dec455cc2473f203a166cfed6cd82e0460
Samir Talwar <stalwar@palantir.com>
2013-11-12 22:25:07 +0000

    Assignment #1: Duplicating the minimum value does not change it.

--- 01 - QuickCheck/src/main/scala/quickcheck/QuickCheck.scala
+++ 01 - QuickCheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -14,8 +14,18 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(h) == a
   }
 
-  lazy val genHeap: Gen[H] = ???
+  property("inserting another value equal to the minimum value does not change the minimum value") = forAll { heap: H =>
+    val min =
+      if (isEmpty(heap)) 0
+      else findMin(heap)
+    findMin(insert(min, heap)) == min
+  }
+
+  lazy val genHeap: Gen[H] = Gen.sized { size =>
+    for {
+      items <- listOfN(size, arbitrary[Int])
+    } yield items.foldLeft(empty)((heap, value) => insert(value, heap))
+  }
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
-
 }
