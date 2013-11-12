a592ede5963e2a214c61bdbfe27abc4e557cf344
Samir Talwar <samir@noodlesandwich.com>
2013-11-12 22:31:27 +0000

    Assignment #1: A new minimum value is retained.

--- 01 - QuickCheck/src/main/scala/quickcheck/QuickCheck.scala
+++ 01 - QuickCheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -21,9 +21,17 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(insert(min, heap)) == min
   }
 
+  property("inserting a value less than the minimum value replaces that as the new minimum value") = forAll { heap: H =>
+    val min =
+      if (isEmpty(heap)) 0
+      else findMin(heap)
+    val newMin = min - 1
+    (min >= 0) ==> (findMin(insert(newMin, heap)) == newMin)
+  }
+
   lazy val genHeap: Gen[H] = Gen.sized { size =>
     for {
-      items <- listOfN(size, arbitrary[Int])
+      items <- listOfN(size, posNum[Int])
     } yield items.foldLeft(empty)((heap, value) => insert(value, heap))
   }
 
