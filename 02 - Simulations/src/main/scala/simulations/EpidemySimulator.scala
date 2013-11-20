def8d4f0dd94ef5f0545d10e6ff92f971d646db0
Samir Talwar <samir@noodlesandwich.com>
2013-11-20 02:07:01 +0000

    Assignment #2: Make the people move.

--- 02 - Simulations/src/main/scala/simulations/EpidemySimulator.scala
+++ 02 - Simulations/src/main/scala/simulations/EpidemySimulator.scala
@@ -16,7 +16,7 @@ class EpidemySimulator extends Simulator {
 
   import SimConfig._
 
-  val persons: List[Person] = List() // to complete: construct list of persons
+  val persons: List[Person] = (0 until population).map(new Person(_)).toList
 
   class Person (val id: Int) {
     var infected = false
@@ -28,8 +28,34 @@ class EpidemySimulator extends Simulator {
     var row: Int = randomBelow(roomRows)
     var col: Int = randomBelow(roomColumns)
 
-    //
-    // to complete with simulation logic
-    //
+    def wrap(f: (Int, Int) => (Int, Int))(row: Int, col: Int) = {
+      val (newRow, newCol) = f(row, col)
+      ((newRow + roomRows) % roomRows, (newCol + roomColumns) % roomColumns)
+    }
+
+    val Directions = List(
+      wrap((r: Int, c: Int) => (r,     c - 1)) _,
+      wrap((r: Int, c: Int) => (r + 1, c    )) _,
+      wrap((r: Int, c: Int) => (r,     c + 1)) _,
+      wrap((r: Int, c: Int) => (r - 1, c    )) _
+    )
+
+    def act() {
+      val possibleMovements = Directions.map(d => d(row, col)).filter { case (newRow, newCol) =>
+        persons.filter(p => p.row == newRow && p.col == newCol).forall(p => !p.sick)
+      }
+      if (!possibleMovements.isEmpty) {
+        val movement = possibleMovements(randomBelow(possibleMovements.size))
+        row = movement._1
+        col = movement._2
+      }
+      actSoon()
+    }
+
+    def actSoon() = afterDelay(randomBelow(5) + 1) {
+      act()
+    }
+
+    actSoon()
   }
 }
