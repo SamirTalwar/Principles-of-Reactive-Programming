9b94e6b1cacd2dfa59e6cfbaa4133de3e22933d0
Samir Talwar <samir@noodlesandwich.com>
2013-12-01 22:27:30 +0000

    Assignment #3: Failed attempt to implement the `main` function.

--- 03 - NodeScala/src/main/scala/nodescala/Main.scala
+++ 03 - NodeScala/src/main/scala/nodescala/Main.scala
@@ -4,7 +4,6 @@ import scala.language.postfixOps
 import scala.concurrent._
 import scala.concurrent.duration._
 import ExecutionContext.Implicits.global
-import scala.async.Async.{async, await}
 
 object Main {
 
@@ -13,27 +12,31 @@ object Main {
     // 1. instantiate the server at 8191, relative path "/test",
     //    and have the response return headers of the request
     val myServer = new NodeScala.Default(8191)
-    val myServerSubscription = ???
+    val myServerSubscription = myServer.start("/test")(request => request.iterator.flatMap { case (name, results) => results })
 
     // TO IMPLEMENT
     // 2. create a future that expects some user input `x`
     //    and continues with a `"You entered... " + x` message
-    val userInterrupted: Future[String] = ???
+    val userInterrupted: Future[String] = Future {
+      blocking {
+        io.Source.stdin.bufferedReader().readLine()
+      }
+    }.map(input => "You entered... " + input)
 
     // TO IMPLEMENT
     // 3. create a future that completes after 20 seconds
     //    and continues with a `"Server timeout!"` message
-    val timeOut: Future[String] = ???
+    val timeOut: Future[String] = Future.delay(20 seconds).continue(_ => "Server timeout!")
 
     // TO IMPLEMENT
     // 4. create a future that completes when either 10 seconds elapse
     //    or the user enters some text and presses ENTER
-    val terminationRequested: Future[String] = ???
+    val terminationRequested: Future[String] = Future.any(List(userInterrupted, timeOut))
 
     // TO IMPLEMENT
     // 5. unsubscribe from the server
     terminationRequested onSuccess {
-      case msg => ???
+      case msg => myServerSubscription.unsubscribe()
     }
   }
 
