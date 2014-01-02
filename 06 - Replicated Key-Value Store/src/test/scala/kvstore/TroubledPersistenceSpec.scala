b50b71e14eefd79fbb138730493a4ad4b14d4583
Samir Talwar <stalwar@palantir.com>
2014-01-02 04:02:30 +0000

    Assignment #6: Assert a troubled persistence layer won't stop secondaries.

--- 06 - Replicated Key-Value Store/src/test/scala/kvstore/TroubledPersistenceSpec.scala
+++ 06 - Replicated Key-Value Store/src/test/scala/kvstore/TroubledPersistenceSpec.scala
@@ -1,6 +1,6 @@
 package kvstore
 
-import kvstore.Arbiter.{JoinedPrimary, Join}
+import kvstore.Arbiter.{Join, JoinedPrimary, JoinedSecondary, Replicas}
 import akka.actor.ActorSystem
 import akka.testkit.{ImplicitSender, TestKit, TestProbe}
 import org.junit.runner.RunWith
@@ -16,10 +16,10 @@ class TroubledPersistenceSpec extends TestKit(ActorSystem("TroubledPersistenceSp
   with ShouldMatchers
   with ImplicitSender
   with Tools {
-  
+
   test("a broken persistence layer won't stop a Primary") {
     val arbiter = TestProbe()
-    val primary = system.actorOf(Replica.props(arbiter.ref, Persistence.props(flaky = true)), "primary")
+    val primary = system.actorOf(Replica.props(arbiter.ref, Persistence.props(flaky = true)), "case1-primary")
     val client = session(primary)
 
     arbiter.expectMsg(Join)
@@ -31,6 +31,31 @@ class TroubledPersistenceSpec extends TestKit(ActorSystem("TroubledPersistenceSp
 
     for (i <- 1 to 20) {
       client.get(s"key-$i").value should be (s"Value $i")
+    }
+  }
+
+  test("a broken persistence layer won't stop a Primary with Secondaries") {
+    val arbiter = TestProbe()
+    val primary = system.actorOf(Replica.props(arbiter.ref, Persistence.props(flaky = true)), "case2-primary")
+    val secondaries = (1 to 1).map(i => system.actorOf(Replica.props(arbiter.ref, Persistence.props(flaky = true)), s"case2-secondary-$i")).toSet
+    val replicas = Seq(primary) ++ secondaries
+    val primaryClient = session(primary)
+    val secondaryClients = secondaries.map(secondary => session(secondary))
+    val clients = Seq(primaryClient) ++ secondaryClients
+
+    replicas foreach { _ => arbiter.expectMsg(Join) }
+    arbiter.send(primary, JoinedPrimary)
+    secondaries foreach { secondary => arbiter.send(secondary, JoinedSecondary) }
+    arbiter.send(primary, Replicas(Set(primary) ++ secondaries))
+
+    for (i <- 1 to 20) {
+      primaryClient.setAcked(s"key-$i", s"Value $i")
+    }
+
+    for (i <- 1 to 20) {
+      for (client <- clients) {
+        client.get(s"key-$i").value should be (s"Value $i")
+      }
     }
   }
 }
