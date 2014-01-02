c152161243e9c479ecd08209e5210ebef761897f
Samir Talwar <stalwar@palantir.com>
2014-01-02 02:37:12 +0000

    Assignment #6: Move the ID into the replication context.

--- 06 - Replicated Key-Value Store/src/main/scala/kvstore/Replicator.scala
+++ 06 - Replicated Key-Value Store/src/main/scala/kvstore/Replicator.scala
@@ -14,47 +14,45 @@ object Replicator {
 
   def props(replica: ActorRef): Props = Props(new Replicator(replica))
   
-  case class ReplicationContext(originator: ActorRef, resender: Cancellable)
+  case class ReplicationContext(id: Long, originator: ActorRef, resender: Cancellable)
 }
 
 class Replicator(val replica: ActorRef) extends Actor {
   import Replicator._
   import context.dispatcher
 
-  private var seqToId = mutable.Map.empty[Long, Long]
   private var replications = mutable.Map.empty[Long, ReplicationContext]
 
   /* TODO Behavior for the Replicator. */
   def receive = replicator(0)
 
   override def postStop() {
-    replications.values foreach { case ReplicationContext(_, resender) =>
+    replications.values foreach { case ReplicationContext(_, _, resender) =>
       resender.cancel()
     }
   }
 
   private def replicator(sequenceCounter: Long): Receive = {
     case Replicate(key, valueOption, id) =>
+      replications(sequenceCounter) = ReplicationContext(id, sender, null)
       sendSnapshot(key, valueOption, id, sequenceCounter)
-      seqToId += sequenceCounter -> id
       context.become(replicator(sequenceCounter + 1))
 
     case ReReplicate(key, valueOption, id, seq) =>
       sendSnapshot(key, valueOption, id, seq)
 
     case SnapshotAck(key, seq) =>
-      val ReplicationContext(originator, resender) = replications(seq)
-      originator ! Replicated(key, seqToId(seq))
+      val ReplicationContext(id, originator, resender) = replications(seq)
+      originator ! Replicated(key, id)
       resender.cancel()
 
       replications -= seq
-      seqToId -= seq
   }
 
   private def sendSnapshot(key: String, valueOption: Option[String], id: Long, seq: Long) {
-    val ReplicationContext(originator, _) = replications.getOrElse(seq, ReplicationContext(sender, null))
+    val ReplicationContext(id, originator, _) = replications(seq)
     replica ! Snapshot(key, valueOption, seq)
     val cancellable = context.system.scheduler.scheduleOnce(200.milliseconds, self, ReReplicate(key, valueOption, id, seq))
-    replications += seq -> ReplicationContext(originator, cancellable)
+    replications += seq -> ReplicationContext(id, originator, cancellable)
   }
 }
