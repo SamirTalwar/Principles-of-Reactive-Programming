4c63a14aa6fc123752779d5f2564fbf346fef3d1
Samir Talwar <stalwar@palantir.com>
2014-01-02 04:00:41 +0000

    Assignment #6: Get rid of the ReReplicate state. It's no longer necessary.

--- 06 - Replicated Key-Value Store/src/main/scala/kvstore/Replicator.scala
+++ 06 - Replicated Key-Value Store/src/main/scala/kvstore/Replicator.scala
@@ -7,7 +7,6 @@ import akka.event.LoggingReceive
 
 object Replicator {
   case class Replicate(key: String, valueOption: Option[String], id: Long)
-  case class ReReplicate(key: String, valueOption: Option[String], id: Long, seq: Long)
   case class Replicated(key: String, id: Long)
   
   case class Snapshot(key: String, valueOption: Option[String], seq: Long)
@@ -35,12 +34,9 @@ class Replicator(val replica: ActorRef) extends Actor {
 
   private def replicator(sequenceCounter: Long): Receive = LoggingReceive {
     case Replicate(key, valueOption, id) =>
-      val schedule = context.system.scheduler.schedule(0.milliseconds, 100.milliseconds, self, ReReplicate(key, valueOption, id, sequenceCounter))
+      val schedule = context.system.scheduler.schedule(0.milliseconds, 100.milliseconds, replica, Snapshot(key, valueOption, sequenceCounter))
       replications(sequenceCounter) = ReplicationContext(id, sender, schedule)
       context.become(replicator(sequenceCounter + 1))
-
-    case ReReplicate(key, valueOption, id, seq) =>
-      replica ! Snapshot(key, valueOption, seq)
 
     case SnapshotAck(key, seq) =>
       if (replications.contains(seq)) {
